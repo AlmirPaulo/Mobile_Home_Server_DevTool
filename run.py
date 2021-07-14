f59c6741ff02aebc16bb8a7432af6e1c648fbda6
AlmirPaulo <ap.freelas@gmail.com>
2021-07-13 22:58:57 -0300

    a correction

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from mobile_dev_server import main
+import main
 
 if __name__ == '__main__':  
    main.app.run(debug=True, port=main.choose_port(main.args.port), host=main.choose_host(main.args.host))
